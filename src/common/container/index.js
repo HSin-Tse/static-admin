8979da711eaecd996eccc5b80829fd3c0649771d
hexinze <hexinze@ajmide.com>
2018-02-26 17:49:39 +0800

    init

---
+++ src/common/container/index.js
@@ -0,0 +1,116 @@
+import React from 'react';
+import {Link} from 'react-router-dom'
+import {Menu, Icon, Switch, Layout} from 'antd'
+import {allMenu} from 'utils/menu'
+import Top from './header'
+import Contents from './content'
+import Footer from './bottom'
+import './index.less'
+
+const SubMenu = Menu.SubMenu;
+const {Sider} = Layout
+
+export default class Container extends React.Component {
+    state = {
+        theme: 'dark',
+        current: 'index',
+        collapsed: false,
+        mode: 'inline',  // 水平垂直展现
+    }
+
+    componentDidMount() {
+        this.handleClick([], 'index')
+    }
+
+    changeTheme = (value) => {
+        this.setState({
+            theme: value ? 'dark' : 'light',
+        });
+    }
+    toggle = () => {
+        this.setState({
+            collapsed: !this.state.collapsed,
+            mode: this.state.collapsed ? 'inline' : 'vertical',
+        });
+    }
+    clear = () => {
+        this.setState({
+            current: 'index',
+        });
+    }
+    handleClick = (e, special) => {
+        this.setState({
+            current: e.key || special,
+        });
+    }
+
+    render() {
+        return (
+            <Layout className="containAll">
+                <Sider
+                    collapsible
+                    collapsed={this.state.collapsed}
+                    onCollapse={this.onCollapse}
+                    className="leftMenu"
+                >
+                    {this.state.theme === 'light' ?
+
+                        <a href="http://121.40.161.184" target='_blank' rel='noopener noreferrer'>
+                            <img src={require('../images/face.png')} width="100" alt="logo" className="lastPic" />
+                            <Icon type="github" className="github"/>
+                        </a> :
+
+                        <a href="http://121.40.161.184" target='_blank' rel='noopener noreferrer'>
+                            <img src={require('../images/favicon.ico')} width="40" alt="logo" className="github circular--squareP" />
+                        </a>}
+                    {this.state.theme === 'light' ? <span className="author">ajmide</span> :
+                        <span className="author white">ajmide</span>}
+                    <Menu
+                        theme={this.state.theme}
+                        onClick={this.handleClick}
+                        defaultOpenKeys={['']}
+                        selectedKeys={[this.state.current]}
+                        className="menu"
+                        mode={this.state.mode}
+                    >
+                        {
+                            allMenu.map((subMenu) => {
+                                if (subMenu.children && subMenu.children.length) {
+                                    return (
+                                        <SubMenu key={subMenu.url} title={<span><Icon
+                                            type={subMenu.icon}/><span>{subMenu.name}</span></span>}>
+                                            {subMenu.children.map(menu => (
+                                                <Menu.Item key={menu.url}><Link
+                                                    to={`/${menu.url}`}>{menu.name}</Link></Menu.Item>
+                                            ))}
+                                        </SubMenu>
+                                    )
+                                }
+                                return (
+                                    <Menu.Item key={subMenu.url}>
+                                        <Link to={`/${subMenu.url}`}>
+                                            <Icon type={subMenu.icon}/><span className="nav-text">{subMenu.name}</span>
+                                        </Link>
+                                    </Menu.Item>
+                                )
+                            })
+                        }
+                    </Menu>
+                    {/*<div className="switch">*/}
+                    {/*<Switch*/}
+                    {/*checked={this.state.theme === 'dark'}*/}
+                    {/*onChange={this.changeTheme}*/}
+                    {/*checkedChildren="Dark"*/}
+                    {/*unCheckedChildren="Light"*/}
+                    {/*/>*/}
+                    {/*</div>*/}
+                </Sider>
+                <Layout>
+                    <Top toggle={this.toggle} collapsed={this.state.collapsed} clear={this.clear}/>
+                    <Contents/>
+                    <Footer/>
+                </Layout>
+            </Layout>
+        );
+    }
+}
---
+++ src/common/pages/h5md/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './index.less'
+
+export default class Gallery extends React.Component {
+
+    render() {
+
+        return (
+            <div className="mdd"   >
+                <iframe className="markdown" src="https://tse-ting.herokuapp.com/h5"></iframe>
+
+            </div>
+        )
+    }
+}
