eaaf5f474242378daeb4dafea429cc8fd976e055
hexinze <hexinze@ajmide.com>
2018-03-02 15:03:36 +0800

    移除index

--- src/common/pages/test/index.js
+++ src/common/pages/test/index.js
@@ -4,7 +4,14 @@ import axios from "axios";
 
 import ReactPlayer from 'react-player'
 import Clock from 'components/clock'
-import {Button,Input, List, Avatar, Icon} from 'antd';
+import {Button, Input, Progress, Layout} from 'antd';
+
+const {Header, Footer, Sider, Content} = Layout;
+const MULTIPLE_SOURCES = [
+    {src: 'http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4', type: 'video/mp4'},
+    {src: 'http://clips.vorwaerts-gmbh.de/big_buck_bunny.ogv', type: 'video/ogv'},
+    {src: 'http://clips.vorwaerts-gmbh.de/big_buck_bunny.webm', type: 'video/webm'}
+]
 
 export default class Tse extends React.Component {
     constructor(props) {
@@ -15,11 +22,13 @@ export default class Tse extends React.Component {
             liked: false,
             playing: true,
             loaded: 0,
+            per: 0,
             playbackRate: 1.0,
             musicurl: 'http://rscdn.ajmide.com/c_250/250.m3u8',
             programs: []
         };
     }
+
     load = url => {
         this.setState({
             url,
@@ -27,6 +36,7 @@ export default class Tse extends React.Component {
             loaded: 0
         })
     }
+
     componentWillMount() {
         console.log("tse componentWillMount");
     }
@@ -61,64 +71,98 @@ export default class Tse extends React.Component {
     };
     handelChange = (e) => {
         this.setState({playing: !this.state.playing})
-        this.setState({playbackRate:e.target.value})
+        this.setState({playbackRate: e.target.value})
     };
     changeSong = (program, e) => {
         console.log('tse changeSong: ')
         this.setState({musicurl: program.liveUrl})
     };
     setPlaybackRate = e => {
-        this.setState({ playbackRate: parseFloat(e.target.value) })
+        this.setState({playbackRate: parseFloat(e.target.value)})
     }
 
     onSeekMouseDown = e => {
-        this.setState({ seeking: true })
+        this.setState({seeking: true})
     }
     onSeekChange = e => {
-        this.setState({ played: parseFloat(e.target.value) })
+        this.setState({played: parseFloat(e.target.value)})
     }
     onSeekMouseUp = e => {
-        this.setState({ seeking: false })
+        this.setState({seeking: false})
         this.player.seekTo(parseFloat(e.target.value))
     }
+    onDuration = (duration) => {
+        console.log('onDuration', duration)
+        this.setState({duration})
+    }
+    onProgress = state => {
+        console.log('onProgress', state)
+        // We only want to update time slider if we are not currently seeking
+        if (!this.state.seeking) {
+            this.setState(state)
+        }
+    }
+
+    ref = player => {
+        this.player = player
+    }
+
     render() {
         const {musicurl, playing, volume, muted, loop, played, loaded, duration, playbackRate, programs} = this.state
 
         var text = this.state.liked ? '喜欢' : '不喜欢';
         const pgs = programs.map((program) =>
-            <li onClick={this.changeSong.bind(this, program)} key={program.name}>{program.name}
-                : {program.liveUrl}  </li>
+            <div>
+                <h3 onClick={this.changeSong.bind(this, program)} key={program.name}>{program.name}
+                    : {program.liveUrl}  </h3>
+                <img style={{height: '100px', width: '100px'}}  src={program.imgPath}></img>
+            </div>
         );
         return (
 
             <div className="mdd">
-                <Clock tsee="asdf"></Clock>
-                <ReactPlayer width='10%'
-                             height='10%'
-                             url={musicurl}
-                             playing={playing}
-                             playbackRate={playbackRate}
-                             onDuration={this.onDuration}
-                             onProgress={this.onProgress}
-                />
-                <Button onClick={this.playPause}>{playing ? 'Pause' : 'Play'}</Button>
-                <Button type="danger" onClick={this.handleClick}>
-                    {text}
-                </Button>
-                <Input  width='50px' type="number" onChange={this.handelChange} value={playbackRate} />
-                <Input
-                    type='range' min={0} max={1} step='any'
-                    value={played}
-                    // onMouseDown={this.onSeekMouseDown}
-                    // onChange={this.onSeekChange}
-                    // onMouseUp={this.onSeekMouseUp}
-                />
-                <progress max={1} value={loaded} />
-                {/*<Button onClick={this.setPlaybackRate} value={playbackRate}>{playbackRate}</Button>*/}
-                <h1 ref="tse" className="red">{this.state.stra}</h1>
-                <ul>{pgs}</ul>
-                <h1>播放速度 {playbackRate}</h1>
-                <h1>{musicurl}</h1>
+                <Layout>
+
+                    <Sider style={{background: '#fff', margin: '20px'}}>
+                        <div>
+                            <Clock tsee="asdf"></Clock>
+                            <ReactPlayer width='10%'
+                                         height='10%'
+                                         ref={this.ref}
+                                         url={musicurl}
+                                         playing={playing}
+                                         playbackRate={playbackRate}
+                                         onDuration={this.onDuration}
+                                         onProgress={this.onProgress}
+                            />
+                            <Button onClick={this.playPause}>{playing ? 'Pause' : 'Play'}</Button>
+                            <Button type="danger" onClick={this.handleClick}>
+                                {text}
+                            </Button>
+                            <Input width='50px' type="number" onChange={this.handelChange} value={playbackRate}/>
+                            <Input
+                                type='range' min={0} max={1} step='any'
+                                value={played}
+                                onMouseDown={this.onSeekMouseDown}
+                                onChange={this.onSeekChange}
+                                onMouseUp={this.onSeekMouseUp}
+                            />
+                            <h3>load</h3>
+                            <Progress type="circle" percent={Math.ceil(loaded * 100)}/>
+                            <h3>Now</h3>
+                            <Progress type="circle" percent={Math.ceil(played * 100)}/>
+                            <h4>播放速度: {playbackRate}</h4>
+                            <h4>musicurl: {musicurl}</h4>
+                            <h4>loaded: {loaded}</h4>
+                            <h4>duration: {Math.round(duration) / 60} 分</h4>
+                        </div>
+                    </Sider>
+                    <Layout>
+                        <Content style={{background: '#fff', margin: '20px'}}>
+                            {pgs}
+                        </Content>
+                    </Layout>
+                </Layout>
             </div>
         )
     }
